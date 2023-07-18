cc88d5597866540ad1f298154b64923c5e112ea1
Khushboo <devkhushboo999@gmail.com>
2023-07-18 12:10:30 +0530

    issue resolved

--- main.go
+++ main.go
@@ -61,7 +61,7 @@ func updateMovie(w http.ResponseWriter, r *http.Request){
 		if item.ID==params["id"]{
 			movies = append(movies[:index], movies[index+1:]...)
 			var movie Movie
-	_=json.NewDecoder(r.Body).Decode(movie)
+	_=json.NewDecoder(r.Body).Decode(&movie)
 	movie.ID=strconv.Itoa(rand.Intn(100000000))
 	movies=append(movies,movie)
 	json.NewEncoder(w).Encode(movie)
@@ -71,7 +71,7 @@ func updateMovie(w http.ResponseWriter, r *http.Request){
 func createMovie(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type","application/json")
 	var movie Movie
-	_=json.NewDecoder(r.Body).Decode(movie)
+	_=json.NewDecoder(r.Body).Decode(&movie)
 	movie.ID=strconv.Itoa(rand.Intn(100000000))
 	movies=append(movies,movie)
 	json.NewEncoder(w).Encode(movie)
